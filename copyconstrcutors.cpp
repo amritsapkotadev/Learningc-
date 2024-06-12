fffb63cc8a2fcd06f6fc5ab3991c315439861d1c
amritsapkota <amritsapkta.dev@gmail.com>
2024-06-12 23:02:58 +0545

    changes

---
+++ copyconstrcutors.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+
+// class number{
+//      int a;
+//      public:
+//      number(int num){
+//           a=num;
+//      }
+//      void display(){
+//           cout<<"the number of this object is "<<a<<endl;;
+     
+//      }
+// };
+// int main(){
+//      number
+// }
